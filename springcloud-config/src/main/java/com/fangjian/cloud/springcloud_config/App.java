7326ad7cab2da6286ba7acfbc596be3b879a08e7
fangjianzy <yyjcf1986@163.com>
2017-07-31 09:25:16 +0800

    t exit

---
+++ springcloud-config/src/main/java/com/fangjian/cloud/springcloud_config/App.java
@@ -0,0 +1,13 @@
+package com.fangjian.cloud.springcloud_config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
